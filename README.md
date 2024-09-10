2683b6a53068ab0159b916b14b394e640451e379
Hanshal Bhatiya <108007617+hanshal18@users.noreply.github.com>
2024-09-10 15:10:49 +0000

    made calculator input text on right and created ui of pomodoro timer and updated readme

--- README.md
+++ README.md
@@ -12,3 +12,4 @@ Later, you'll build a shopping cart and other applications to learn how to creat
 - [random quote machine](https://hanshal18-random-quote-machine.netlify.app/)
 - [markdown previewer](https://hanshal18-markdown-previewer.netlify.app/)
 - [drum machine](https://hanshal18-drum-machine.netlify.app)
+- [calculator](https://hanshal18-calculator.netlify.app)
--- pomodoro-timer/src/app/app.component.ts
+++ pomodoro-timer/src/app/app.component.ts
@@ -1,13 +1,64 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import Timer from "easytimer.js";
+//import FormModule from '@angular/forms'
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet],//FormModule
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
+  timer = new Timer();
+
+  change(change:any,type:any){
+    let breaklength = document.getElementById('break-length') as HTMLInputElement
+    let sessionlength = document.getElementById('session-length') as HTMLInputElement
+    if(type=='break'){
+      if(change=="+"){
+        if(breaklength.value=="60") return;
+        breaklength.value = `${parseInt(breaklength.value)+1}`
+      }else if(change=="-"){
+        if(breaklength.value=="0") return;
+        breaklength.value = `${parseInt(breaklength.value)-1}`
+      }
+    }else if(type=='session'){
+      if(change=="+"){
+        if(sessionlength.value=="60") return;
+        sessionlength.value = `${parseInt(sessionlength.value)+1}`
+      }else if(change=="-"){
+        if(sessionlength.value=="0")return;
+        sessionlength.value = `${parseInt(sessionlength.value)-1}`
+      }
+    }
+  }
+  runTimer(){
+    let timer = document.getElementById('start_stop') as HTMLButtonElement
+    let timeleft= document.getElementById('timer-left') as HTMLDivElement
+    let sessionlength = document.getElementById('session-length') as HTMLInputElement
+    if(timer.innerHTML.includes('play')){
+      if(sessionlength.value+":00"==timeleft.innerText){
+        this.timer.start({countdown:true, startValues:{minutes:parseInt(sessionlength.value)}});
+      }else{
+        this.timer.start()
+      }
+
+      timer.innerHTML=`<img src="./pause.png" alt="pause">`
+      setInterval(() => {
+        timeleft.innerText= this.timer.getTimeValues().minutes.toString()+":"+this.timer.getTimeValues().seconds.toString()
+      }, 1000);
+    }else if(timer.innerHTML.includes('pause')){
+      this.timer.pause();
+      timer.innerHTML=`<img src="./play.png" alt="play">`
+    }
+  }
+  reset(){
+    let breaklength = document.getElementById('break-length') as HTMLInputElement
+    let sessionlength = document.getElementById('session-length') as HTMLInputElement
+    breaklength.value = "5"
+    sessionlength.value = "25"
+  }
   title = 'pomodoro-timer';
 }
