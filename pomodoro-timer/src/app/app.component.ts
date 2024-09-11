7835ac26323189666d9a29d00fe9461915977aea
Hanshal Bhatiya <108007617+hanshal18@users.noreply.github.com>
2024-09-11 06:06:18 +0000

    pomodoro timer done

--- pomodoro-timer/src/app/app.component.ts
+++ pomodoro-timer/src/app/app.component.ts
@@ -1,64 +1,76 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import Timer from "easytimer.js";
-//import FormModule from '@angular/forms'
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],//FormModule
+  imports: [RouterOutlet,FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   timer = new Timer();
-
+  breakLength = 5;
+  sessionLength = 25;
+  whatsrunning = 'session'
   change(change:any,type:any){
-    let breaklength = document.getElementById('break-length') as HTMLInputElement
-    let sessionlength = document.getElementById('session-length') as HTMLInputElement
     if(type=='break'){
       if(change=="+"){
-        if(breaklength.value=="60") return;
-        breaklength.value = `${parseInt(breaklength.value)+1}`
+        if(this.breakLength==60) return;
+        this.breakLength += 1
       }else if(change=="-"){
-        if(breaklength.value=="0") return;
-        breaklength.value = `${parseInt(breaklength.value)-1}`
+        if(this.breakLength==0) return;
+        this.breakLength -= 1
       }
     }else if(type=='session'){
       if(change=="+"){
-        if(sessionlength.value=="60") return;
-        sessionlength.value = `${parseInt(sessionlength.value)+1}`
+        if(this.sessionLength==60) return;
+        this.sessionLength += 1
       }else if(change=="-"){
-        if(sessionlength.value=="0")return;
-        sessionlength.value = `${parseInt(sessionlength.value)-1}`
+        if(this.sessionLength==0)return;
+        this.sessionLength -= 1
       }
     }
   }
+
   runTimer(){
     let timer = document.getElementById('start_stop') as HTMLButtonElement
     let timeleft= document.getElementById('timer-left') as HTMLDivElement
-    let sessionlength = document.getElementById('session-length') as HTMLInputElement
+    let audio = document.querySelector('audio') as HTMLAudioElement
+
     if(timer.innerHTML.includes('play')){
-      if(sessionlength.value+":00"==timeleft.innerText){
-        this.timer.start({countdown:true, startValues:{minutes:parseInt(sessionlength.value)}});
+      if(this.whatsrunning=="session"){
+        this.timer.start({countdown:true, startValues:{minutes:this.sessionLength}});
+        
+      }else if(this.whatsrunning=="break"){
+        this.timer.start({countdown:true, startValues:{minutes:this.breakLength}});
       }else{
         this.timer.start()
       }
-
-      timer.innerHTML=`<img src="./pause.png" alt="pause">`
-      setInterval(() => {
+      this.timer.addEventListener('secondsUpdated', (e)=>{
         timeleft.innerText= this.timer.getTimeValues().minutes.toString()+":"+this.timer.getTimeValues().seconds.toString()
-      }, 1000);
+      })
+      this.timer.addEventListener('targetAchieved', (e)=> {
+        this.whatsrunning=="session"? this.whatsrunning="break" : this.whatsrunning="session";
+        audio.play();
+        if(this.whatsrunning=="break"){
+          this.timer.start({countdown:true, startValues:{minutes:this.breakLength}});
+        }else if(this.whatsrunning=="session"){
+          this.timer.start({countdown:true, startValues:{minutes:this.sessionLength}});
+        }
+      });
+      timer.innerHTML=`<img src="./pause.png" alt="pause">`
     }else if(timer.innerHTML.includes('pause')){
       this.timer.pause();
       timer.innerHTML=`<img src="./play.png" alt="play">`
     }
   }
-  reset(){
-    let breaklength = document.getElementById('break-length') as HTMLInputElement
-    let sessionlength = document.getElementById('session-length') as HTMLInputElement
-    breaklength.value = "5"
-    sessionlength.value = "25"
+
+  reset(){  
+    this.breakLength = 5
+    this.sessionLength = 25
   }
   title = 'pomodoro-timer';
 }
